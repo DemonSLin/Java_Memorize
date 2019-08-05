c2db48daeabb0528c73aff3948e5de6784aba6ff
SongLin <2448673097@qq.com>
2019-08-05 20:17:53 +0800

    每隔两个数删除一个数并输出最后一个数下标

---
+++ my_practice/DelArray.java
@@ -0,0 +1,25 @@
+package www.lsl.com.Aug;
+//每隔两个数删除一个数，循环删除，输出最后一个数下标
+import java.util.*;
+public class DelArray {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        while (sc.hasNext()) {
+            int n = sc.nextInt();
+            if (n > 1000) {
+                n = 999;
+            }
+            List<Integer> list = new ArrayList<Integer>();
+            for (int i = 0; i < n; i++) {
+                list.add(i);
+            }
+            int i = 0;
+            while (list.size() > 1) {
+                i = (i + 2) % list.size();
+                list.remove(i);
+            }
+            System.out.println(list.get(0));
+        }
+    }
+}
+
